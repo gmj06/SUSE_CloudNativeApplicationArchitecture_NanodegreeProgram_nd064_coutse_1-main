fefc85d7b87aaeb270ab78203e7d6b57189c3f4c
GMJ06 <gowrimj2006@gmail.com>
2021-09-13 22:14:20 -0700

    Added healthz and metrics endpoint in app.py and also created Dockerfile

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -2,12 +2,19 @@ import sqlite3
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+import logging
+
+# Define the Flask application
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'SUSECloudNativeApp'
+app.config['DB_CONNECTION_COUNT'] = 0
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    app.config['DB_CONNECTION_COUNT'] += 1
     return connection
 
 # Function to get a post using its ID
@@ -18,9 +25,40 @@ def get_post(post_id):
     connection.close()
     return post
 
-# Define the Flask application
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your secret key'
+
+
+# Define Healthcheck endpoint
+@app.route('/healthz')
+def healthchecks():
+    response = app.response_class(
+        response=json.dumps({"result": "OK - healthy"}),
+        status = 200,
+        mimetype='application/json'
+    )
+    app.logger.info('HealthCheck request successful!')
+    return response
+
+# Define Metrics endpoint
+@app.route('/metrics')
+def metrics():
+    connection = get_db_connection()
+    posts = connection.execute('SELECT * FROM posts').fetchall()
+    connection.close()
+    response =  app.response_class(
+        response=json.dumps({
+            "status":"success", "code": 0, "data": {
+                "db_connection_count":app.config['DB_CONNECTION_COUNT'],
+                "post_counnt": len(posts)
+            }
+        }),
+        status=200,
+        mimetype='application/json'
+    )
+    app.logger.info('Metrics request successful!')
+    return response
+
+
+
 
 # Define the main route of the web application 
 @app.route('/')
@@ -36,13 +74,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      return render_template('404.html'), 404
+        app.logger.info('Article does not exists!')
+        return render_template('404.html'), 404
     else:
-      return render_template('post.html', post=post)
+        app.logger.info('Article "' + post.title + '" retrieved!')
+        return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info('The "About Us" page is retrieved!')
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -61,6 +102,7 @@ def create():
             connection.commit()
             connection.close()
 
+            app.logger.info('Article "' + title + '" created succesfully!"')
             return redirect(url_for('index'))
 
     return render_template('create.html')
